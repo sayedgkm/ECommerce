e8184c95b564effd377bf67eb2fb9f861f63be4e
Arif <ahb.arif@gmail.com>
2022-11-12 20:34:51 +0600

    Added product related components

--- ECommerceClient/src/app/app.module.ts
+++ ECommerceClient/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ProductInventoryComponent } from './product/product-inventory/product-inventory.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
+import { ProductDetailComponent } from './product/product-detail/product-detail.component';
+import { ProductFilterComponent } from './product/product-filter/product-filter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProductInventoryComponent,
+    ProductListComponent,
+    ProductDetailComponent,
+    ProductFilterComponent
   ],
   imports: [
     BrowserModule,
---
+++ ECommerceClient/src/app/product/product-inventory/product-inventory.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-inventory',
+  templateUrl: './product-inventory.component.html',
+  styleUrls: ['./product-inventory.component.css']
+})
+export class ProductInventoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
